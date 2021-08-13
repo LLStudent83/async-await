6dbb9c1f69d4ba22bc227026e9b2c519f5a3e0e0
Dmitry Prigulnov <prigulnov@mail.ru>
2021-08-13 22:45:09 +0500

    Исправил тест

--- src/js/__tests__/async.test.js
+++ src/js/__tests__/async.test.js
@@ -2,5 +2,11 @@ import GameSavingLoader from '../gameSavingLoader_await';
 
 
 test('checking return promise', () => GameSavingLoader.load().then((data) => {
-  expect(data).toBe('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
+  expect(data).toEqual({
+    created: 1546300800,
+    id: 9,
+    userInfo: {
+      id: 1, level: 10, name: 'Hitman', points: 2000,
+    },
+  });
 }));
